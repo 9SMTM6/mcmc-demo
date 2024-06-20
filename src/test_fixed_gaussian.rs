50805ce702c7c480bb4dca76b46b7a20fe90735e
Samuel Maier <samuel.maier2@hotmail.de>
2024-06-20 14:30:08 +0200

    Added non-working modifications to test_fixed_gaussian

--- src/test_fixed_gaussian.rs
+++ src/test_fixed_gaussian.rs
@@ -1,23 +1,54 @@
+use std::{mem::size_of_val, num::NonZeroU64};
+
 use eframe::egui_wgpu::{CallbackTrait, RenderState};
-use wgpu::{FragmentState, RenderPipeline, RenderPipelineDescriptor, ShaderModuleDescriptor, VertexState};
+use wgpu::{util::DeviceExt, BindGroup, Buffer};
+use wgpu::{
+    FragmentState, RenderPipeline, RenderPipelineDescriptor, ShaderModuleDescriptor, VertexState,
+};
 
 struct GaussPipeline {
     pipeline: RenderPipeline,
+    bind_group: BindGroup,
+    uniform_buffer: Buffer,
 }
 
 #[derive(Clone, Copy)]
-pub struct FixedGaussian{}
+pub struct FixedGaussian {}
+
+const RENDER_SIZE: [f32; 2] = [64000.0, 48000.0];
 
 impl FixedGaussian {
     pub fn new(render_state: &RenderState) -> Self {
-        let dev = &render_state.device;
+        let device = &render_state.device;
 
-        let shader = dev.create_shader_module(ShaderModuleDescriptor {
-            label: None,
+        let webgpu_debug_name = Some("test_gaussian");
+
+        let shader = device.create_shader_module(ShaderModuleDescriptor {
+            label: webgpu_debug_name,
             source: wgpu::ShaderSource::Wgsl(include_str!("test_fixed_gaussian.wgsl").into()),
         });
 
-        let pipeline = dev.create_render_pipeline(&RenderPipelineDescriptor {
+        let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            label: webgpu_debug_name,
+            entries: &[wgpu::BindGroupLayoutEntry {
+                binding: 0,
+                visibility: wgpu::ShaderStages::FRAGMENT,
+                ty: wgpu::BindingType::Buffer {
+                    ty: wgpu::BufferBindingType::Uniform,
+                    has_dynamic_offset: false,
+                    min_binding_size: NonZeroU64::new(size_of_val(&RENDER_SIZE) as u64),
+                },
+                count: None,
+            }],
+        });
+
+        let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+            label: webgpu_debug_name,
+            bind_group_layouts: &[&bind_group_layout],
+            push_constant_ranges: &[],
+        });
+
+        let pipeline = device.create_render_pipeline(&RenderPipelineDescriptor {
             vertex: VertexState {
                 module: &shader,
                 buffers: &[],
@@ -30,18 +61,42 @@ impl FixedGaussian {
                 entry_point: "fs_main",
                 targets: &[Some(render_state.target_format.into())],
             }),
-            label: None,
-            layout: None,
+            label: webgpu_debug_name,
+            layout: Some(&pipeline_layout),
             depth_stencil: None,
             multisample: Default::default(),
             multiview: Default::default(),
             primitive: Default::default(),
         });
 
+        let uniform_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: webgpu_debug_name,
+            contents: bytemuck::cast_slice(&RENDER_SIZE),
+            usage: wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::UNIFORM,
+        });
+
+        let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: webgpu_debug_name,
+            layout: &bind_group_layout,
+            entries: &[wgpu::BindGroupEntry {
+                binding: 0,
+                resource: uniform_buffer.as_entire_binding(),
+            }],
+        });
+
         // Because the graphics pipeline must have the same lifetime as the egui render pass,
         // instead of storing the pipeline in our struct, we insert it into the
         // `callback_resources` type map, which is stored alongside the render pass.
-        let None = render_state.renderer.write().callback_resources.insert(GaussPipeline { pipeline }) else {
+        let None = render_state
+            .renderer
+            .write()
+            .callback_resources
+            .insert(GaussPipeline {
+                pipeline,
+                bind_group,
+                uniform_buffer,
+            })
+        else {
             panic!("pipeline already present?!")
         };
 
@@ -50,16 +105,34 @@ impl FixedGaussian {
 }
 
 impl CallbackTrait for FixedGaussian {
+    fn prepare(
+        &self,
+        _device: &wgpu::Device,
+        queue: &wgpu::Queue,
+        _screen_descriptor: &eframe::egui_wgpu::ScreenDescriptor,
+        _egui_encoder: &mut wgpu::CommandEncoder,
+        callback_resources: &mut eframe::egui_wgpu::CallbackResources,
+    ) -> Vec<wgpu::CommandBuffer> {
+        let GaussPipeline { uniform_buffer, .. } = callback_resources.get().unwrap();
+        queue.write_buffer(&uniform_buffer, 0, bytemuck::cast_slice(&RENDER_SIZE));
+        Vec::new()
+    }
+
     fn paint<'a>(
-            &'a self,
-            _info: egui::PaintCallbackInfo,
-            render_pass: &mut wgpu::RenderPass<'a>,
-            callback_resources: &'a eframe::egui_wgpu::CallbackResources,
-        ) {
-        let GaussPipeline {pipeline} = callback_resources.get().unwrap();
+        &'a self,
+        _info: egui::PaintCallbackInfo,
+        render_pass: &mut wgpu::RenderPass<'a>,
+        callback_resources: &'a eframe::egui_wgpu::CallbackResources,
+    ) {
+        let GaussPipeline {
+            pipeline,
+            bind_group,
+            ..
+        } = callback_resources.get().unwrap();
 
         render_pass.set_pipeline(pipeline);
-        render_pass.draw(0..6, 0..1);
+        render_pass.set_bind_group(0, bind_group, &[]);
+        render_pass.draw(0..1, 0..1);
     }
 }
 
@@ -67,10 +140,14 @@ impl FixedGaussian {
     pub fn draw(&self, ui: &mut egui::Ui) {
         egui::Frame::canvas(ui.style()).show(ui, |ui| {
             // ([0.0, 0.0].into()
-            let rect = egui::Rect::from_min_size(ui.cursor().min, egui::Vec2::splat(800.0));
+            let rect = egui::Rect::from_min_size(ui.cursor().min, egui::Vec2::from(RENDER_SIZE));
             // let (rect, response) =
             //     ui.allocate_exact_size(egui::Vec2::splat(300.0), egui::Sense::drag());
-            ui.painter().add(eframe::egui_wgpu::Callback::new_paint_callback(rect, self.clone()))
+            ui.painter()
+                .add(eframe::egui_wgpu::Callback::new_paint_callback(
+                    rect,
+                    self.clone(),
+                ))
         });
     }
 }
--- src/test_fixed_gaussian.wgsl
+++ src/test_fixed_gaussian.wgsl
@@ -1,39 +1,50 @@
-var<private> v_positions: array<vec2<f32>, 6> = array(
+@group(0) @binding(0) var<uniform> resolution: vec2<f32>;
+
+const len = 5;
+
+var<private> gauss_centers: array<vec2<f32>, len> = array(
     vec2(0.0, 0.0),
-    vec2(0.0, 1.0),
-    vec2(0.5, 0.5),
-    vec2(1.0, 0.0),
-    vec2(1.0, 1.0),
-    vec2(0.0, 1.0),
+    vec2(0.2, 0.4),
+    vec2(0.9, 0.3),
+    vec2(0.1, 0.6),
+    vec2(1.0, 0.5),
 );
 
-var<private> v_colors: array<vec4<f32>, 6> = array(
-    vec4(1.0, 0.0, 0.0, 1.0),
-    vec4(1.0, 0.0, 0.0, 1.0),
-    vec4(0.0, 1.0, 0.0, 1.0),
-    vec4(1.0, 0.0, 0.0, 1.0),
-    vec4(1.0, 0.0, 0.0, 1.0),
-    vec4(1.0, 0.0, 0.0, 1.0),
+const gauss_scales: array<f32, len> = array(
+    .5,
+    .6,
+    .8,
+    .2,
+    .5,
 );
 
-struct VertexOut {
-    @interpolate(perspective) @location(0) color: vec4<f32>,
-    @builtin(position) position: vec4<f32>,
-};
+const gauss_variance: array<f32, len> = array(
+    .5,
+    .2,
+    .5,
+    .8,
+    .6,
+);
+
+// struct VertexOut {
+//     color: vec4<f32>,
+//     @builtin(position) position: vec4<f32>,
+// };
 
 @vertex
-fn vs_main(@builtin(vertex_index) v_idx: u32) -> VertexOut {
-    var out: VertexOut;
-
-    out.position = vec4<f32>(v_positions[v_idx], 0.0, 1.0);
-    out.color = v_colors[v_idx];
-
-    return out;
+fn vs_main(@builtin(vertex_index) v_idx: u32) -> @builtin(position) vec4<f32> {
+    return vec4(0.0);
 }
 
 @fragment
-fn fs_main(in: VertexOut) -> @location(0) vec4<f32> {
-    return in.color;
+fn fs_main(@builtin(position) pixel_coords: vec4<f32>) -> @location(0) vec4<f32> {
+    let normalized_coords = pixel_coords.xy / max(resolution.x, resolution.y) * 2 - 1;
+    // let normalized_coords = vec2(
+    //     pixel_coords.x / max(resolution.x, resolution.y) * 2 - 1.0,
+    //     pixel_coords.y / max(resolution.x, resolution.y) * 2 - 1.0,
+    // );
+
+    let color = 1 - distance(normalized_coords, vec2(0.0));
+    // vec3(color)
+    return vec4(vec3(color), 0.0);
 }
-
-
