6a671c0bc960834a169588a7a2c5094543837a96
Samuel Maier <samuel.maier2@hotmail.de>
2024-06-20 18:39:51 +0200

    replace public egui template references

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ fn main() -> eframe::Result<()> {
         ..Default::default()
     };
     eframe::run_native(
-        "eframe template",
+        "mcmc-demo",
         native_options,
         Box::new(|cc| Ok(Box::new(mcmc_demo::TemplateApp::new(cc)))),
     )
