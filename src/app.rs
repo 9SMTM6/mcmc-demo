d27310ca4689c7164c645e861f5c5aaf5e2458c8
Samuel Maier <samuel.maier2@hotmail.de>
2024-06-27 09:15:36 +0200

    Re-enable persistence, if compiled in

--- src/app.rs
+++ src/app.rs
@@ -35,10 +35,10 @@ impl TemplateApp {
 
         // Load previous app state (if any).
         // Note that you must enable the `persistence` feature for this to work.
-        // #[cfg(feature = "persistence")]
-        // if let Some(storage) = cc.storage {
-        //     return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
-        // }
+        #[cfg(feature = "persistence")]
+        if let Some(storage) = cc.storage {
+            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
+        }
 
         Default::default()
     }
