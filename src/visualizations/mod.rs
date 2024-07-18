e643d628ffcfff9050deb144856128cb84dcc2a6
Samuel Maier <samuel.maier2@hotmail.de>
2024-07-18 14:38:45 +0200

    rename appropriately

--- src/visualizations/mod.rs
+++ src/visualizations/mod.rs
@@ -1,9 +1,9 @@
-pub use egui_expressed::{Arrow, PredictionVariance, SamplingPoint};
+pub use egui_based::{Arrow, PredictionVariance, SamplingPoint};
 
-mod egui_expressed;
-mod shaders;
+mod egui_based;
+mod shader_based;
 
-pub use shaders::{multimodal_gaussian::MultiModalGaussian, INITIAL_RENDER_SIZE};
+pub use shader_based::{multimodal_gaussian::MultiModalGaussian, INITIAL_RENDER_SIZE};
 
 pub trait CanvasPainter {
     fn paint(&self, painter: &egui::Painter, rect: egui::Rect);
