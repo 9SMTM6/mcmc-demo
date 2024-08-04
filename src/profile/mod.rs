e8e96feb1eeb5d3bd1fc0e03ac27a3a57d934477
Samuel Maier <samuel.maier2@hotmail.de>
2024-08-04 21:49:17 +0200

    address lints

--- src/profile/mod.rs
+++ src/profile/mod.rs
@@ -17,30 +17,35 @@ macro_rules! profile_scope {
 
 #[cfg(feature = "profile")]
 mod if_featured {
+    #[allow(clippy::missing_const_for_fn, reason = "false positive if compiling for wasm")]
     pub fn start_puffin_server() {
-        puffin::set_scopes_on(true); // tell puffin to collect data
+        // puffin server doesnt exist on web, so make that a noop there
+        #[cfg(not(target_arch = "wasm32"))]
+        {
+            puffin::set_scopes_on(true); // tell puffin to collect data
 
-        match puffin_http::Server::new("127.0.0.1:8585") {
-            Ok(puffin_server) => {
-                eprintln!(
-                    "Run:  cargo install puffin_viewer && puffin_viewer --url 127.0.0.1:8585"
-                );
+            match puffin_http::Server::new("127.0.0.1:8585") {
+                Ok(puffin_server) => {
+                    eprintln!(
+                        "Run:  cargo install puffin_viewer && puffin_viewer --url 127.0.0.1:8585"
+                    );
 
-                std::process::Command::new("puffin_viewer")
-                    .arg("--url")
-                    .arg("127.0.0.1:8585")
-                    .spawn()
-                    .ok();
+                    std::process::Command::new("puffin_viewer")
+                        .arg("--url")
+                        .arg("127.0.0.1:8585")
+                        .spawn()
+                        .ok();
 
-                // We can store the server if we want, but in this case we just want
-                // it to keep running. Dropping it closes the server, so let's not drop it!
-                #[allow(clippy::mem_forget, reason = "we want to keep it running")]
-                std::mem::forget(puffin_server);
-            }
-            Err(err) => {
-                eprintln!("Failed to start puffin server: {err}");
-            }
-        };
+                    // We can store the server if we want, but in this case we just want
+                    // it to keep running. Dropping it closes the server, so let's not drop it!
+                    #[allow(clippy::mem_forget, reason = "we want to keep it running")]
+                    std::mem::forget(puffin_server);
+                }
+                Err(err) => {
+                    eprintln!("Failed to start puffin server: {err}");
+                }
+            };
+        }
     }
 }
 
