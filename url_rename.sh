10ca87a856d9820a1a8834e6ba07aff21c78e517
Samuel Maier <samuel.maier2@hotmail.de>
2024-08-07 20:24:06 +0200

    rework deployment

--- url_rename.sh
+++ url_rename.sh
@@ -5,9 +5,8 @@ set -eo
 
 # will effectively be executed in the dist folder generated with `trunk build --release --public-url $(<"github_url.txt")`
 
-# Assign arguments to variables
-old_url=$(<"github_url.txt")
-new_url=$CF_PAGES_URL
+old_url=__RENAME_ME__
+new_url=$1
 
 # Find and replace the URL
 find "$PWD" -type f -exec sed -i "s|$old_url|$new_url|Ig" {} +
