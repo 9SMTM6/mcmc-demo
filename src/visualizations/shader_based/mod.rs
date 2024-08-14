d8de319134286b298448c4f8b2e583c3c8a67eb9
Samuel Maier <samuel.maier2@hotmail.de>
2024-08-14 11:09:22 +0200

    remove unneccesary lints (including an update to patched wgsl_to_wgpu, which neccessitated a cargo relock)

--- src/visualizations/shader_based/mod.rs
+++ src/visualizations/shader_based/mod.rs
@@ -16,21 +16,11 @@ macro_rules! create_shader_module {
     ($shader_name:expr, $module_name: ident) => {
         #[allow(
             unused,
-            non_snake_case,
-            non_camel_case_types,
-            non_upper_case_globals,
             elided_lifetimes_in_paths,
-            clippy::redundant_static_lifetimes,
             clippy::approx_constant,
-            clippy::needless_borrow,
-            clippy::undocumented_unsafe_blocks,
-            clippy::missing_const_for_fn,
             clippy::module_name_repetitions,
             clippy::pattern_type_mismatch,
             clippy::unreadable_literal,
-            clippy::wrong_self_convention,
-            clippy::allow_attributes_without_reason,
-            clippy::used_underscore_binding
         )]
         pub mod $module_name {
             include!(concat!(
