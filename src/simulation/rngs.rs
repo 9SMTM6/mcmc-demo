7095dc8aaa36181615eaf7cd431ac35cf7c5be78
Samuel Maier <samuel.maier2@hotmail.de>
2024-08-21 12:14:51 +0200

    clean up somewhat, apply features correctly

--- src/simulation/rngs.rs
+++ src/simulation/rngs.rs
@@ -1,12 +1,8 @@
 use std::borrow::Borrow;
 
 use egui::Slider;
-use rand::{Rng, RngCore, SeedableRng};
-// #[cfg(feature="rng_small")]
-// use rand::rngs::SmallRng;
+use rand::{Rng, SeedableRng};
 use rand_distr::{StandardNormal, Uniform};
-use rand_pcg;
-use strum::{EnumMessage, VariantArray};
 
 macro_rules! declare_rng_wrapper_macro {
     ($macro_name: ident, mod $path: tt$(, feature = $feature: literal)?) => {
@@ -39,20 +35,32 @@ macro_rules! declare_rng_wrappers {
         $(
             create_rng_wrapper_xorshift!(struct $xorshift_rng);
         )+
-
+        
         pub enum WrappedRng {
             $($pcg_rng(::rand_pcg::$pcg_rng),)+
-            $($xoshiro_rng(::rand_xoshiro::$xoshiro_rng),)+
-            $($xorshift_rng(::rand_xorshift::$xorshift_rng),)+
+            $(
+                #[cfg(feature = "rng_xoshiro")]
+                $xoshiro_rng(::rand_xoshiro::$xoshiro_rng),
+            )+
+            $(
+                #[cfg(feature = "rng_xorshift")]
+                $xorshift_rng(::rand_xorshift::$xorshift_rng),
+            )+
         }
-
+        
         #[derive(PartialEq, Clone, Copy)]
         pub enum WrappedRngDiscriminants {
             $($pcg_rng,)+
-            $($xoshiro_rng,)+
-            $($xorshift_rng,)+
+            $(
+                #[cfg(feature = "rng_xoshiro")]
+                $xoshiro_rng,
+            )+
+            $(
+                #[cfg(feature = "rng_xorshift")]
+                $xorshift_rng,
+            )+
         }
-
+        
         impl WrappedRngDiscriminants {
             pub const VARIANTS: [WrappedRngDiscriminants; 10] = [$(Self::$pcg_rng),+, $(Self::$xoshiro_rng),+, $(Self::$xorshift_rng),+];
             pub fn seed_from_u64(&self, seed: u64) -> WrappedRng {
@@ -60,17 +68,29 @@ macro_rules! declare_rng_wrappers {
                 use WrappedRng as T;
                 match *self {
                     $(D::$pcg_rng => T::$pcg_rng(::rand_pcg::$pcg_rng::seed_from_u64(seed)),)+
-                    $(D::$xoshiro_rng => T::$xoshiro_rng(::rand_xoshiro::$xoshiro_rng::seed_from_u64(seed)),)+
-                    $(D::$xorshift_rng => T::$xorshift_rng(::rand_xorshift::$xorshift_rng::seed_from_u64(seed)),)+
+                    $(
+                        #[cfg(feature = "rng_xoshiro")]
+                        D::$xoshiro_rng => T::$xoshiro_rng(::rand_xoshiro::$xoshiro_rng::seed_from_u64(seed)),
+                    )+
+                    $(
+                        #[cfg(feature = "rng_xorshift")]
+                        D::$xorshift_rng => T::$xorshift_rng(::rand_xorshift::$xorshift_rng::seed_from_u64(seed)),
+                    )+
                 }
             }
-
+            
             pub fn display_name(&self) -> &'static str {
                 use WrappedRngDiscriminants as D;
                 match *self {
                     $(D::$pcg_rng => stringify!($pcg_rng),)+
-                    $(D::$xoshiro_rng => stringify!($xoshiro_rng),)+
-                    $(D::$xorshift_rng => stringify!($xorshift_rng),)+
+                    $(
+                        #[cfg(feature = "rng_xoshiro")]
+                        D::$xoshiro_rng => stringify!($xoshiro_rng),
+                    )+
+                    $(
+                        #[cfg(feature = "rng_xorshift")]
+                        D::$xorshift_rng => stringify!($xorshift_rng),
+                    )+
                 }
             }
         }
@@ -80,8 +100,14 @@ macro_rules! declare_rng_wrappers {
                 use WrappedRngDiscriminants as D;
                 match value {
                     $(T::$pcg_rng(_) => D::$pcg_rng),+,
-                    $(T::$xoshiro_rng(_) => D::$xoshiro_rng),+,
-                    $(T::$xorshift_rng(_) => D::$xorshift_rng),+
+                    $(
+                        #[cfg(feature = "rng_xoshiro")]
+                        T::$xoshiro_rng(_) => D::$xoshiro_rng
+                    ),+,
+                    $(
+                        #[cfg(feature = "rng_xorshift")]
+                        T::$xorshift_rng(_) => D::$xorshift_rng
+                    ),+
                 }
             }
         }
@@ -123,15 +149,15 @@ pub struct SRngGaussianIter<Rng> {
 
 impl<R> Iterator for SRngGaussianIter<R>
 where
-    R: Rng + SeedableRng,
+R: Rng + SeedableRng,
 {
     type Item = f32;
-
+    
     #[inline(always)]
     fn next(&mut self) -> Option<f32> {
         Some(self.rng.sample(StandardNormal))
     }
-
+    
     fn size_hint(&self) -> (usize, Option<usize>) {
         (usize::MAX, None)
     }
@@ -158,15 +184,15 @@ pub struct SRngPercIter<Rng> {
 
 impl<R> Iterator for SRngPercIter<R>
 where
-    R: Rng + SeedableRng,
+R: Rng + SeedableRng,
 {
     type Item = f32;
-
+    
     #[inline(always)]
     fn next(&mut self) -> Option<f32> {
         Some(self.rng.sample(self.distr))
     }
-
+    
     fn size_hint(&self) -> (usize, Option<usize>) {
         (usize::MAX, None)
     }
@@ -179,7 +205,7 @@ impl<R: Rng + SeedableRng> SRngPercIter<R> {
             distr: Uniform::new_inclusive(0.0, 1.0),
         }
     }
-
+    
     pub fn unwrapped_next(&mut self) -> f32 {
         self.next().expect("infinite iterator")
     }
@@ -192,7 +218,7 @@ impl WrappedRngDiscriminants {
             //     continue;
             // }
             ui.selectable_value(self, ele, ele.display_name())
-                .on_hover_text(ele.explanation());
+            .on_hover_text(ele.explanation());
         }
     }
 }
@@ -211,25 +237,24 @@ impl WrappedRng {
                 seed: 42,
             })
         });
-
+        
         current_settings.discr.selection_ui(ui);
         ui.add(Slider::new(&mut current_settings.seed, 0..=u64::MAX));
-
+        
+        if ui.button("apply").clicked() {
+            *self = current_settings.discr.seed_from_u64(current_settings.seed);
+        }
+        
         ui.data_mut(|type_map| {
             type_map.insert_temp(id, current_settings);
         });
-
-        if ui.button("apply").clicked() {
-            todo!();
-        //     *self = AdoptedRngs::from(current_settings.discr);
-        }
     }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-
+    
     #[test]
     fn sizeof_rand() {
         // TODO: update to enum
