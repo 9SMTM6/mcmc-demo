559484e744417df6b506948007c40a452814eaef
Samuel Maier <samuel.maier2@hotmail.de>
2024-08-22 18:10:29 +0200

    move html_bindings to new helpers dir-module

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 mod app;
 mod bg_task;
-pub mod html_bindings;
+mod helpers;
 pub mod profile;
 mod settings;
 mod simulation;
@@ -10,6 +10,7 @@ pub use app::McmcDemo;
 #[cfg(feature = "tracing")]
 pub use profile::tracing::{define_subscriber, set_default_and_redirect_log};
 pub use visualizations::INITIAL_RENDER_SIZE;
+pub use helpers::html_bindings;
 
 #[cfg(not(any(feature = "rng_pcg", feature = "rng_xorshift", feature = "rng_xoshiro")))]
 compile_error!("no rng compiled in.");
