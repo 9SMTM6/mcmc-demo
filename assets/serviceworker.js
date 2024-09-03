9bd42e1b313f19dd979d43655029ff621acc76e3
Samuel Maier <samuel.maier2@hotmail.de>
2024-09-03 23:45:47 +0200

    improve serviceworker: try to refresh unhashed files

--- assets/serviceworker.js
+++ assets/serviceworker.js
@@ -1,34 +1,58 @@
 const CACHE_NAME = 'mmcmc-demo-v1';
-const STATIC_FILES_REGEX = [
-    /^\/$/,
-    /^\/(index\.html)$/,
+const HASHED_FILES = [
     /^\/mcmc-demo-[a-f0-9]{16}_bg\.wasm$/,
     /^\/mcmc-demo-[a-f0-9]{16}\.js$/,
     /^\/snippets\/wasm-bindgen-futures-[a-f0-9]{16}\/src\/task\/worker.js$/,
+    // TODO: really I'd like to have the webpage icon not hashed with how stuff is handled.
+    // But it doesn't seem trunk offers that option.
     /^\/favicon-[a-f0-9]{16}\.svg$/,
-    /^\/favicon.svg$/,
-    /^\/manifest.json$/,
 ];
+const UNHASHED_FILES = [
+    "/",
+    "/index.html",
+    "/favicon.svg",
+    "/manifest.json",
+]
 
 // Fetch event: Serve cached files, and cache new ones with hashes
 self.addEventListener('fetch', (event) => {
     const url = new URL(event.request.url);
 
     if (url.origin === location.origin) {
-        const matchingRegex = STATIC_FILES_REGEX.find(regex => regex.test(url.pathname));
+        const matchingFilename = UNHASHED_FILES.find(filename => url.pathname === filename);
+        const matchingRegexOrFilename = matchingFilename ?? HASHED_FILES.find(regex => regex.test(url.pathname));
 
-        if (matchingRegex) {
+        if (matchingRegexOrFilename) {
             event.respondWith(
                 caches.match(event.request).then((cachedResponse) => {
-                    // If cached, return it
+                    // If cached, directly return hashed files, however try refreshing unhashed files.
                     if (cachedResponse) {
-                        return cachedResponse;
+                        // if its a hashed file, returning it is (pretty much, exclusing hash conflicts which I'll ignore) save to just serve the cached file.
+                        if (!matchingFilename) {
+                            return cachedResponse;
+                        } else {
+                            // If the element is not hashed for cache invalidation, try to refresh the cache.
+                            return fetch(event.request).then((networkResponse) => {
+                                // If the network response is ok, cache the response
+                                if (networkResponse.ok) {
+                                    caches.open(CACHE_NAME).then((cache) => {
+                                        cache.put(event.request, networkResponse.clone());
+                                    });
+                                    return networkResponse;
+                                }
+                                // If the network request fails, return the cached response (if available)
+                                return cachedResponse || networkResponse;
+                            }).catch(() => {
+                                // If network fetch fails return cached response
+                                return cachedResponse;
+                            });
+                        }
                     }
                     // Else, fetch from network and cache the response
                     return caches.open(CACHE_NAME).then((cache) => {
-                        return fetch(event.request).then((response) => {
-                            cache.put(event.request, response.clone());
-                            return response;
+                        return fetch(event.request).then((networkResponse) => {
+                            cache.put(event.request, networkResponse.clone());
+                            return networkResponse;
                         });
                     });
                 })
