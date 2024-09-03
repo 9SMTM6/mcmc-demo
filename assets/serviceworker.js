f1308ec675eedf93a44de50c806e5bd9605fe5e9
Samuel Maier <samuel.maier2@hotmail.de>
2024-09-04 00:08:15 +0200

    refactor serviceworker to async await, with small logic improvements

--- assets/serviceworker.js
+++ assets/serviceworker.js
@@ -17,46 +17,37 @@ const UNHASHED_FILES = [
 // Fetch event: Serve cached files, and cache new ones with hashes
 self.addEventListener('fetch', (event) => {
     const url = new URL(event.request.url);
-
+    
     if (url.origin === location.origin) {
         const matchingFilename = UNHASHED_FILES.find(filename => url.pathname === filename);
         const matchingRegexOrFilename = matchingFilename ?? HASHED_FILES.find(regex => regex.test(url.pathname));
-
+        
         if (matchingRegexOrFilename) {
-            event.respondWith(
-                caches.match(event.request).then((cachedResponse) => {
-                    // If cached, directly return hashed files, however try refreshing unhashed files.
-                    if (cachedResponse) {
-                        // if its a hashed file, returning it is (pretty much, exclusing hash conflicts which I'll ignore) save to just serve the cached file.
-                        if (!matchingFilename) {
-                            return cachedResponse;
-                        } else {
-                            // If the element is not hashed for cache invalidation, try to refresh the cache.
-                            return fetch(event.request).then((networkResponse) => {
-                                // If the network response is ok, cache the response
-                                if (networkResponse.ok) {
-                                    caches.open(CACHE_NAME).then((cache) => {
-                                        cache.put(event.request, networkResponse.clone());
-                                    });
-                                    return networkResponse;
-                                }
-                                // If the network request fails, return the cached response (if available)
-                                return cachedResponse || networkResponse;
-                            }).catch(() => {
-                                // If network fetch fails return cached response
-                                return cachedResponse;
-                            });
-                        }
+            const resolvedPromise = (async () => {
+                const cachedResponse = await caches.match(event.request);
+                if (cachedResponse) {
+                    // if its a hashed file, returning it is (pretty much, exclusing hash conflicts which I'll ignore) save to just serve the cached file.
+                    if (!matchingFilename) {
+                        return cachedResponse;
+                    } 
+                    // If the element is not hashed for cache invalidation, try to refresh the cache.
+                    let networkResponse = await fetch(event.request).catch(() => cachedResponse);
+                    if (!networkResponse.ok) {
+                        return cachedResponse;
                     }
-                    // Else, fetch from network and cache the response
-                    return caches.open(CACHE_NAME).then((cache) => {
-                        return fetch(event.request).then((networkResponse) => {
-                            cache.put(event.request, networkResponse.clone());
-                            return networkResponse;
-                        });
+                    caches.open(CACHE_NAME).then((cache) => {
+                        cache.put(event.request, networkResponse.clone());
                     });
-                })
-            );
+                    return networkResponse;
+                }
+                const [cache, networkResponse] = await Promise.all([
+                    caches.open(CACHE_NAME),
+                    fetch(event.request),
+                ]);
+                cache.put(event.request, networkResponse.clone());
+                return networkResponse;
+            })();
+            event.respondWith(resolvedPromise);
         } else {
             // Handle other requests normally (or apply different caching strategy)
             event.respondWith(fetch(event.request));
