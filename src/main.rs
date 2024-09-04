0a054f9431677bce4ecee2d0f9ab3b9b6e3ad295
Samuel Maier <samuel.maier2@hotmail.de>
2024-09-04 21:08:07 +0200

    Failing experiments to include embassy as async executor for both web and native
    For now only code for native, which fails.
    
    It builds, but then upon opening the site, one gets an error on the initial import from "env".
    This seems to be caused by a combination of the web wasm_bindgen target and some `unresolved import` from rust, likely caused by some mangling or not mangling from embassy.
    
    Before this i tried to do this with the async main with attribute, as in the embassy wasm example, but then I got a compiler error that main is there twice.
    Note that the embassy example is using the more usual library rust code approach.
    However my experiments at moving towards that also failed just the same.

--- src/main.rs
+++ src/main.rs
@@ -35,9 +35,42 @@ fn main() -> eframe::Result<()> {
     )
 }
 
+use embassy_time::Timer;
+
+#[embassy_executor::task]
+async fn ticker() {
+    let window = web_sys::window().expect("no global `window` exists");
+
+    let mut counter = 0;
+    loop {
+        let document = window.document().expect("should have a document on window");
+        let list = document.get_element_by_id("log").expect("should have a log element");
+
+        let li = document.create_element("li").expect("error creating list item element");
+        li.set_text_content(Some(&format!("tick {}", counter)));
+
+        list.append_child(&li).expect("error appending list item");
+        log::info!("tick {}", counter);
+        counter += 1;
+
+        Timer::after_secs(1).await;
+    }
+}
+
 // When compiling to web using trunk:
 #[cfg(target_arch = "wasm32")]
 fn main() {
+    let executor = ::std::boxed::Box::leak(
+        ::std::boxed::Box::new(::embassy_executor::Executor::new()),
+    );
+    executor
+        .start(|spawner| {
+            spawner.must_spawn(ticker());
+        });
+    // let mut executor = embassy_executor::Executor::new();
+    // executor.start(|spawner: Spawner| {
+    //     spawner.spawn(ticker()).unwrap();        
+    // });
     use mcmc_demo::html_bindings::*;
 
     // Log or trace to stderr (if you run with `RUST_LOG=debug`).
