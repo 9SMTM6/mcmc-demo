f110dca62a8b0f88833220070ecd4d03413b3be0
Samuel Maier <samuel.maier2@hotmail.de>
2024-09-11 15:45:53 +0200

    Fix pipeline

--- src/visualizations/egui_based/mod.rs
+++ src/visualizations/egui_based/mod.rs
@@ -45,7 +45,7 @@ impl CanvasPainter for Arrow {
             stroke: PathStroke {
                 width: 1.5,
                 color: ColorMode::Solid(Color32::RED),
-                .. Default::default()
+                ..Default::default()
             },
         };
         painter.extend([base, head]);
