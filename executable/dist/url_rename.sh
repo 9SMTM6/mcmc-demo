cc5dd89c42c0879a0ca0875343f34954486652bf
Samuel Maier <samuel.maier2@hotmail.de>
2024-10-20 16:47:57 +0200

    try to fix CF deployment

--- executable/dist/url_rename.sh
+++ executable/dist/url_rename.sh
@@ -1,12 +1,13 @@
-#!/bin/env bash
+#!/bin/env sh
 
-set -x
-set -eo
+# set -x
+# set -eo
 
 # will effectively be executed in the dist folder generated with `trunk build --release --public-url $(<"github_url.txt")`
 
 old_url=__RENAME_ME__
 new_url=$1
+# In old times (this breaks caching for main pages) $CF_PAGES_URL
 
 # Find and replace the URL
 find "$PWD" -type f -exec sed -i "s|$old_url|$new_url|Ig" {} +
