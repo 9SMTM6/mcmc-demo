f65b6d6f4eaaaa6110420f6a5a8afa51486356a0
Samuel Maier <samuel.maier2@hotmail.de>
2024-10-20 17:22:43 +0200

    Re-enable non-breaking parts of url_rename

--- executable/dist/url_rename.sh
+++ executable/dist/url_rename.sh
@@ -1,13 +1,13 @@
 #!/bin/env sh
 
-# set -x
-# set -eo
+set -x
+set -eo
 
 # will effectively be executed in the dist folder generated with `trunk build --release --public-url $(<"github_url.txt")`
 
 old_url=__RENAME_ME__
 new_url=$1
-# In old times (this breaks caching for main pages) $CF_PAGES_URL
+# In old times (this breaks the PWA setup for main pages, but correctly deploys developement pages) $CF_PAGES_URL
 
 # Find and replace the URL
 find "$PWD" -type f -exec sed -i "s|$old_url|$new_url|Ig" {} +
