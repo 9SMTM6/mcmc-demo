e352178d83f51fefba2c1c44dd699590d0d3a950
Samuel Maier <samuel.maier2@hotmail.de>
2024-10-29 18:33:23 +0100

    Update comments in index.html some more

--- executable/dist/url_rename.sh
+++ executable/dist/url_rename.sh
@@ -10,4 +10,6 @@ new_url=$1
 # In old times (this breaks the PWA setup for main pages, but correctly deploys development pages) $CF_PAGES_URL
 
 # Find and replace the URL
-find "$PWD" -type f -exec sed -i "s|$old_url|$new_url|Ig" {} +
+find "$PWD/slim" -type f -exec sed -i "s|$old_url|$new_url/slim|Ig" {} +
+
+find "$PWD/fat" -type f -exec sed -i "s|$old_url|$new_url/fat|Ig" {} +
